eb261b7b384780b46685f87edfbdafc58bb9001a
zhiyongjzy <zhiyongjzy@gmail.com>
2018-07-16 11:29:23 +0800

    update ...

--- python_basic/lesson3/send_text.py
+++ python_basic/lesson3/send_text.py
@@ -2,8 +2,8 @@
 from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/user/account
-account_sid = "AC26429a9c4d7490d604776192fe9e3d6d"
-auth_token = "a66c82d382c4a0fa9030ff1121b52fb5"
+account_sid = ""
+auth_token = ""
 
 
 client = Client(account_sid, auth_token)
